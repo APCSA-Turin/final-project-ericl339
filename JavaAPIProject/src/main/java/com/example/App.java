ac53bc707abc4e75e3ab751573de42058bf20f18
Eric Li <148896243+ericl339@users.noreply.github.com>
2025-05-19 22:56:46 +0000

    project

--- JavaAPIProject/src/main/java/com/example/App.java
+++ JavaAPIProject/src/main/java/com/example/App.java
@@ -7,49 +7,93 @@ import org.json.JSONObject;
 
 public class App {
     public static void main(String[] args) {
-        String bearerCode = null;
-        String output = null;
+
         try {
-            bearerCode = twitchGetter.getBearer();
-            String clientId = twitchGetter.getClientId();
-            String clientSecret = twitchGetter.getClientSecret();
-            System.out.println(clientId);
-            System.out.println(clientSecret);
-            System.out.println(bearerCode);  
+            String bearerCode = twitchGetter.getBearer(); // access code
+            String clientId = twitchGetter.getClientId(); // client id
+            String clientSecret = twitchGetter.getClientSecret(); // client secret
 
-            output = API.getData("https://api.igdb.com/v4/genres/", bearerCode, clientId, "fields *; limit 2;");
+            // String output = API.getData("https://api.igdb.com/v4/genres/", bearerCode, clientId, "fields *; limit 500;");
+            // // i learned this from https://www.tutorialspoint.com/how-to-write-create-a-json-array-using-java
+            // JSONArray genreArray = new JSONArray(output);
 
+            // ArrayList<GenreInfo> genreList = new ArrayList<>();
+            // for (int i = 0; i < genreArray.length(); i++) {
+            //     JSONObject genre = genreArray.getJSONObject(i);
+            //     GenreInfo item = new GenreInfo(genre.getString("name"), genre.getInt("id"));
+            //     genreList.add(item);
+            //     // System.out.println("count: " + (i+1));
+            //     // System.out.println("id: " + genre.getInt("id"));
+            //     // System.out.println("genre: " + genre.getString("name"));
+            // }
+
+            String output = API.getData("https://api.igdb.com/v4/games", bearerCode, clientId, "fields name, genres.*, game_engines.*, involved_companies.*, platforms.*, game_type, summary; where genres != null & game_engines != null & involved_companies != null & platforms != null & summary != null; limit 2;");
+            // System.out.println(output.toString());
             // i learned this from https://www.tutorialspoint.com/how-to-write-create-a-json-array-using-java
-            JSONArray genreArray = new JSONArray(output);
+            JSONArray gameArray = new JSONArray(output);
 
-            ArrayList<GenreInfo> genreList = new ArrayList<>();
+            ArrayList<GameInfo> gameList = new ArrayList<>();
+            for (int i = 0; i < gameArray.length(); i ++) {
+                JSONObject game = gameArray.getJSONObject(i);
 
-            for (int i = 0; i < genreArray.length(); i++) {
+                ArrayList<String> genreName = new ArrayList<>();
+                int categoryId = game.getInt("game_type");
+                ArrayList<String> gameEngineName = new ArrayList<>();
+                ArrayList<String> companyName = new ArrayList<>();
+                ArrayList<String> platformName = new ArrayList<>();
+                String name = game.getString("name");
+                String summary = game.getString("summary");
+                String output1 = API.getData("https://api.igdb.com/v4/game_types", bearerCode, clientId, "fields *; where id = (" + categoryId + ");");
+                JSONArray gameTypeArray = new JSONArray(output1);
+                JSONObject typeInfo = gameTypeArray.getJSONObject(0);
+                String gameType = typeInfo.getString("type");
 
-                JSONObject genre = genreArray.getJSONObject(i);
-                GenreInfo item = new GenreInfo(genre.getString("name"), genre.getInt("id"));
-                genreList.add(item);
+                JSONArray genres = game.getJSONArray("genres");
+                for (int j = 0; j < genres.length(); j ++) {
+                    JSONObject k = genres.getJSONObject(j);
+                    genreName.add(k.getString("name"));
+                }
 
-                System.out.println("count: " + (i+1));
-                System.out.println("id: " + genre.getInt("id"));
-                System.out.println("genre: " + genre.getString("name"));
+                JSONArray engines = game.getJSONArray("game_engines");
+                for (int j = 0; j < engines.length(); j ++) {
+                    JSONObject k = engines.getJSONObject(j);
+                    gameEngineName.add(k.getString("name"));
+                }
+
+                JSONArray companies = game.getJSONArray("involved_companies");
+                for (int j = 0; j < companies.length(); j ++) {
+                    JSONObject k = companies.getJSONObject(j);
+                    int companyId = k.getInt("company");
+                    String output2 = API.getData("https://api.igdb.com/v4/companies", bearerCode, clientId, "fields name; where id = (" + companyId + ");");
+                    JSONArray outputCompany = new JSONArray(output2);
+                    JSONObject y = outputCompany.getJSONObject(0);
+                    companyName.add(y.getString("name"));
+                }
+
+                JSONArray platforms = game.getJSONArray("platforms");
+                for (int j = 0; j < platforms.length(); j ++) {
+                    JSONObject k = platforms.getJSONObject(j);
+                    platformName.add(k.getString("abbreviation"));
+                }
+
+                GameInfo item = new GameInfo(genreName,gameType,gameEngineName,companyName,platformName,name,summary);
+                gameList.add(item);
             }
 
-            output = API.getData("https://api.igdb.com/v4/games", bearerCode, clientId, "fields *; limit 500;");
-            JSONArray gameArray = new JSONArray(output);
-            ArrayList<GameInfo> gameList = new ArrayList<>();
-            
+            for (int i = 0; i < gameList.size(); i ++) {
+                System.out.println("Genre: " + gameList.get(i).getGenreName());
+                System.out.println("Type: " + gameList.get(i).getGameType());
+                System.out.println("Engine: " + gameList.get(i).getGameEngineName());
+                System.out.println("Company Name: " + gameList.get(i).getCompanyName());
+                System.out.println("Platform name: " + gameList.get(i).getplatformName());
+                System.out.println("Name: " + gameList.get(i).getName());
+                System.out.println("Summary: " + gameList.get(i).getSummary());
+                System.out.println();
+            }
 
         } catch (Exception e) {
             System.err.println("Failed to get token: " + e.getMessage());
             e.printStackTrace();
         }
-        // String clientId = twitchGetter.getClientId();
-        // String clientSecret = twitchGetter.getClientSecret();
-        // System.out.println(clientId);
-        // System.out.println(clientSecret);
-        // System.out.println(bearerCode);        
-        // System.out.println();
-        // System.out.println(output);
-        }
+    }
 }
--- JavaAPIProject/src/main/java/com/example/GameInfo.java
+++ JavaAPIProject/src/main/java/com/example/GameInfo.java
@@ -1,21 +1,32 @@
 package com.example;
 
+import java.util.ArrayList;
+
 public class GameInfo {
-    private ArrayList<Integer> genreId;
-    private int categoryId;
-    private ArrayList<Integer> gameEngineId;
-    private ArrayList<Integer> companyId;
-    private ArrayList<Integer> platformId;
+    private ArrayList<String> genreName;
+    private String gameType;
+    private ArrayList<String> gameEngineName;
+    private ArrayList<String> companyName;
+    private ArrayList<String> platformName;
     private String name;
     private String summary;
 
-    public GameInfo(ArrayList<Integer> genreId, int categoryId, ArrayList<Integer> gameEngineId, ArrayList<Integer> companyId, ArrayList<Integer> platformId, String name, String summary) {
-        this.genreId = genreId;
-        this.categoryId = categoryId;
-        this.gameEngineId = gameEngineId;
-        this.companyId = companyId;
-        this.platformId = platformId;
+    public GameInfo(ArrayList<String> genreName, String gameType, ArrayList<String> gameEngineName, ArrayList<String> companyName, ArrayList<String> platformName, String name, String summary) {
+        this.genreName = genreName;
+        this.gameType = gameType;
+        this.gameEngineName = gameEngineName;
+        this.companyName = companyName;
+        this.platformName = platformName;
         this.name = name;
         this.summary = summary;
     }
+
+    public ArrayList<String> getGenreName(){return genreName;}
+    public String getGameType() {return gameType;}
+    public ArrayList<String> getGameEngineName(){return gameEngineName;}
+    public ArrayList<String> getCompanyName(){return companyName;}
+    public ArrayList<String> getplatformName(){return platformName;}
+    public String getName() {return name;}
+    public String getSummary() {return summary;}
+
 }
